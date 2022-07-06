1194edf8df9fa7a2b1f2f6011482df8380663599
FelipeAxel <axelnovais96@gmail.com>
2022-07-05 23:04:07 -0300

    test

--- src/main/java/com/axelfelipe/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/com/axelfelipe/minhasfinancas/MinhasfinancasApplication.java
@@ -3,8 +3,6 @@ package com.axelfelipe.minhasfinancas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
